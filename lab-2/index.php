5a7c7531ee50721b11369418399dc86e813e3855
Ilya Androsov <ilya_androsov@icloud.com>
2020-04-05 22:39:35 +0300

    feat: add the same city removal

--- lab-2/index.php
+++ lab-2/index.php
@@ -24,25 +24,27 @@
         <textarea class="form-container_message" name="cities-input" id="cities-input" cols="60" rows="10"></textarea>
         <input class="form-container__submit" type="submit" value="Sort">
         <?php
-          function mb_ucfirst($string, $encoding)
-          {
-              $strlen = mb_strlen($string, $encoding);
-              $firstChar = mb_substr($string, 0, 1, $encoding);
-              $then = mb_substr($string, 1, $strlen - 1, $encoding);
-              return mb_strtoupper($firstChar, $encoding) . $then;
+
+          function mb_ucfirst($string, $encoding): string {
+            $strlen = mb_strlen($string, $encoding);
+            $firstChar = mb_substr($string, 0, 1, $encoding);
+            $then = mb_substr($string, 1, $strlen - 1, $encoding);
+            return mb_strtoupper($firstChar, $encoding) . $then;
           }
 
           $indexName = "cities-input";
           if( isset($_GET[$indexName]) ) {
-            $inputString = mb_strtolower($_GET[$indexName],"utf8");
+            $inputString = mb_strtolower($_GET[$indexName], "utf8");
             $citiesArray = preg_split("/[\s,]+/", $inputString);
+            $citiesArray = array_unique($citiesArray);
             sort($citiesArray, SORT_STRING);
             $answer = "";
             for ($i = 0; $i < count($citiesArray); $i++) {
-              $answer .= mb_ucfirst($citiesArray[$i],"utf8") . " ";
+              $answer .= mb_ucfirst($citiesArray[$i], "utf8") . " ";
             }
             echo "<label class='form-container__label' for='cities-output'>Sorted cities sequence:</label><textarea class='form-container_message' name='cities-output' id='cities-output' cols='60' rows='10'>$answer</textarea>";
           }
+
         ?>
       </form>
     </main>
