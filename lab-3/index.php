2916c244e0c97165d7532fbe67927402a9d53377
Ilya Androsov <ilya_androsov@icloud.com>
2020-04-16 14:35:33 +0300

    init: add files from previous project

---
+++ lab-3/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="./Styles/Main.css">
+  <title>Ilya Androsov</title>
+</head>
+  <body>
+    <main>
+      <?php 
+        include './Nav.php';
+      ?>
+      <div class="intro-block">
+        <div class="info-block">
+          <h2 class="info-block__description">Hello, glad to see you here! I'm</h2>
+          <h1 class="info-block__name">Ilya Androsov</h1>
+        </div>
+        <a href="#lab-3" class="intro-block__button">Go to lab 3</a>
+      </div>
+      <form action="<?php $_PHP_SELF ?>" method="POST" id="lab-3" class="form-container">
+        <h2 class="form-container__header">Lab 3, variant 1</h2>
+        <label class="form-container__label" for="id-input">Enter id:</label>
+        <input type="text" name="id-input" id="id-input">
+        <label class="form-container__label" for="name-input">Enter name:</label>
+        <input type="text" name="name-input" id="name-input">
+        <label class="form-container__label" for="price-input">Enter price:</label>
+        <input type="number" name="price-input" id="price-input">
+        <label class="form-container__label" for="description-input">Add description:</label>
+        <textarea class="form-container_message" name="description-input" id="description-input" cols="60" rows="10"></textarea>
+        <input class="form-container__submit" type="submit" value="Add to list">
+        <?php
+
+          function mb_ucfirst($string, $encoding): string {
+            $strlen = mb_strlen($string, $encoding);
+            $firstChar = mb_substr($string, 0, 1, $encoding);
+            $then = mb_substr($string, 1, $strlen - 1, $encoding);
+            return mb_strtoupper($firstChar, $encoding) . $then;
+          }
+
+          $indexName = "cities-input";
+          if( isset($_GET[$indexName]) ) {
+            $inputString = mb_strtolower($_GET[$indexName], "utf8");
+            $citiesArray = preg_split("/[\s,]+/", $inputString);
+            $citiesArray = array_unique($citiesArray);
+            sort($citiesArray, SORT_STRING);
+            $answer = "";
+            for ($i = 0; $i < count($citiesArray); $i++) {
+              $answer .= mb_ucfirst($citiesArray[$i], "utf8") . " ";
+            }
+            echo "<label class='form-container__label' for='cities-output'>Sorted cities sequence:</label><textarea class='form-container_message' name='cities-output' id='cities-output' cols='60' rows='10'>$answer</textarea>";
+          }
+
+        ?>
+      </form>
+    </main>
+    <footer class="footer-wrapper">
+      <div class="socials-block">
+        <span class="socials-block__text">Social media:</span>
+        <ul class="socials-block__list">
+          <li class="socials-block__list_item"><a href="https://github.com/Ajololr"><img src="Assets/Icons/github.svg" alt=""></a></li>
+          <li class="socials-block__list_item"><a href="https://vk.com/andrik0099"><img src="Assets/Icons/vk.svg" alt=""></a></li>
+          <li class="socials-block__list_item"><a href="https://www.linkedin.com/in/%D0%B8%D0%BB%D1%8C%D1%8F-%D0%B0%D0%BD%D0%B4%D1%80%D0%BE%D1%81%D0%BE%D0%B2-48ba35198/"><img src="Assets/Icons/linkedin.svg" alt=""></a></li>
+        </ul>
+      </div>
+      <div class="tel-block">
+        <span class="tel-block_text">Telephone:</span>
+        <a class="tel-block__value" href="tel:+375-29-804-26-89">+375-29-804-26-89</a>
+      </div>
+    </footer>
+  </body>
+</html>
