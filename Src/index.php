d6db8040a31a288ca31eef64ecf734de97b4eb12
Ilya Androsov <ilya_androsov@icloud.com>
2020-05-08 00:11:58 +0300

    feat: add transletion for main page

--- Src/index.php
+++ Src/index.php
@@ -1,10 +1,14 @@
+<?php
+  include './Language.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="./Styles/Main.css">
-  <title>Ilya Androsov</title>
+  <link rel="stylesheet" type="text/css" href="./Styles/Language.css">
+  <title><?php echo $textValues["MAIN_PAGE_TITLE"]; ?></title>
 </head>
   <body>
     <main>
@@ -13,25 +17,12 @@
       ?>
       <div class="intro-block">
         <div class="info-block">
-          <h2 class="info-block__description">Hello, glad to see you here! I'm</h2>
-          <h1 class="info-block__name">Ilya Androsov</h1>
+          <h2 class="info-block__description"><?php echo $textValues["GREETINGS"]; ?></h2>
+          <h1 class="info-block__name"><?php echo $textValues["MAIN_PAGE_TITLE"]; ?></h1>
         </div>
-        <a href="./Labs.php" class="intro-block__button">Go to WT labs</a>
+        <a href="./Labs.php" class="intro-block__button"><?php echo $textValues["GO_TO_WT_LABS"]; ?></a>
       </div>
     </main>
-    <footer class="footer-wrapper">
-      <div class="socials-block">
-        <span class="socials-block__text">Social media:</span>
-        <ul class="socials-block__list">
-          <li class="socials-block__list_item"><a href="https://github.com/Ajololr"><img src="Assets/Icons/github.svg" alt=""></a></li>
-          <li class="socials-block__list_item"><a href="https://vk.com/andrik0099"><img src="Assets/Icons/vk.svg" alt=""></a></li>
-          <li class="socials-block__list_item"><a href="https://www.linkedin.com/in/%D0%B8%D0%BB%D1%8C%D1%8F-%D0%B0%D0%BD%D0%B4%D1%80%D0%BE%D1%81%D0%BE%D0%B2-48ba35198/"><img src="Assets/Icons/linkedin.svg" alt=""></a></li>
-        </ul>
-      </div>
-      <div class="tel-block">
-        <span class="tel-block_text">Telephone:</span>
-        <a class="tel-block__value" href="tel:+375-29-804-26-89">+375-29-804-26-89</a>
-      </div>
-    </footer>
+    <?php include './Footer.php'?>
   </body>
 </html>
